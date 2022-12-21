cbf496845de184ef91a72764a8b8f70237d07419
Leonardo Oyama <leonardooyama@gmail.com>
2022-12-21 17:11:34 -0300

    Code cleanup.

--- source/plugin.cpp
+++ source/plugin.cpp
@@ -45,30 +45,23 @@
 XPLMDataRef DataRefFlightModelLat;
 XPLMDataRef DataRefFlightModelLon;
 XPLMDataRef DataRefFlightModelElev;
-XPLMDataRef DataRefFlightModelTrueTheta;
-XPLMDataRef DataRefFlightModelTruePhi;
-XPLMDataRef DataRefFlightModelTruePsi;
-XPLMDataRef DataRefFlightModelMagPsi;
-XPLMDataRef DataRefFlightModelLocalVx;
-XPLMDataRef DataRefFlightModelLocalVy;
-XPLMDataRef DataRefFlightModelLocalVz;
-XPLMDataRef DataRefFlightModelLocalAx;
-XPLMDataRef DataRefFlightModelLocalAy;
-XPLMDataRef DataRefFlightModelLocalAz;
 
 double flightModelLat, flightModelLon, flightModelElev;
-float flightModelTrueTheta, flightModelTruePhi, flightModelTruePsi, flightModelMagPsi;
-float flightModelLocalVx, flightModelLocalVy, flightModelLocalVz;
-float flightModelLocalAx, flightModelLocalAy, flightModelLocalAz;
 
 float FlightLoopSendUDPDatagram(float inElapsedSinceLastCall, float inElapsedTimeSinceLastFlightLoop, int inCounter, void *inRefcon);
 float FlightLoopCheckDatagramReceived(float inElapsedSinceLastCall, float inElapsedTimeSinceLastFlightLoop, int inCounter, void *inRefcon);
 
 XPLMWindowID	gWindow = NULL;
 
+std::string pluginName = "CustomIO_UDP";
+std::string pluginSignature = "coter.CustomIO_UDP";
+std::string pluginDescription = "Plugin to add custom network communication to X-Plane, performing input and ouput of data.";
+
 QUdpSocket *UDPSocketMainSender;
 QUdpSocket *UDPSocketSecondarySender;
 QUdpSocket *UDPSocketReceiver;
+
+void DebugToXPlaneLog(QString debugString);
 
 void MyDrawWindowCallback(
         XPLMWindowID         inWindowID,
@@ -94,9 +87,9 @@ PLUGIN_API int XPluginStart(
         char *	outSig,
         char *	outDesc)
 {
-    strcpy(outName, "CustomIO_UDP");
-    strcpy(outSig, "coter.CustomIO_UDP");
-    strcpy(outDesc, "Plugin to add custom network communication to X-Plane, performing input and ouput of data.");
+    strcpy_s(outName, pluginName.size() + 1, pluginName.c_str());
+    strcpy_s(outSig, pluginSignature.size() + 1, pluginSignature.c_str());
+    strcpy_s(outDesc, pluginDescription.size() + 1, pluginDescription.c_str());
 
     XPLMRegisterFlightLoopCallback(FlightLoopSendUDPDatagram, xplm_FlightLoop_Phase_AfterFlightModel, NULL);
     XPLMRegisterFlightLoopCallback(FlightLoopCheckDatagramReceived, xplm_FlightLoop_Phase_AfterFlightModel, NULL);
@@ -104,29 +97,21 @@ PLUGIN_API int XPluginStart(
     DataRefFlightModelLat = XPLMFindDataRef("sim/flightmodel/position/latitude");
     DataRefFlightModelLon = XPLMFindDataRef("sim/flightmodel/position/longitude");
     DataRefFlightModelElev = XPLMFindDataRef("sim/flightmodel/position/elevation");
-    DataRefFlightModelTrueTheta = XPLMFindDataRef("sim/flightmodel/position/true_theta");
-    DataRefFlightModelTruePhi = XPLMFindDataRef("sim/flightmodel/position/true_phi");
-    DataRefFlightModelTruePsi = XPLMFindDataRef("sim/flightmodel/position/true_psi");
-    DataRefFlightModelMagPsi = XPLMFindDataRef("sim/flightmodel/position/mag_psi");
-    DataRefFlightModelLocalVx = XPLMFindDataRef("sim/flightmodel/position/local_vx");
-    DataRefFlightModelLocalVy = XPLMFindDataRef("sim/flightmodel/position/local_vy");
-    DataRefFlightModelLocalVz = XPLMFindDataRef("sim/flightmodel/position/local_vz");
-    DataRefFlightModelLocalAx = XPLMFindDataRef("sim/flightmodel/position/local_ax");
-    DataRefFlightModelLocalAy = XPLMFindDataRef("sim/flightmodel/position/local_ay");
-    DataRefFlightModelLocalAz = XPLMFindDataRef("sim/flightmodel/position/local_az");
     UDPSocketMainSender = new QUdpSocket();
     UDPSocketSecondarySender = new QUdpSocket();
     UDPSocketReceiver = new QUdpSocket();
     bool bindSuccess = UDPSocketReceiver->bind(QHostAddress::AnyIPv4, 15000, QAbstractSocket::ReuseAddressHint);
+    QString debugString;
     if (bindSuccess)
     {
-        XPLMDebugString("Success binding UDP port!\n");
+        debugString = "Success binding UDP port!";
+        DebugToXPlaneLog(debugString);
     }
     else
     {
-        XPLMDebugString("Failed binding UDP port!\n");
+        debugString = "Failed binding UDP port!";
+        DebugToXPlaneLog(debugString);
     }
-
     return 1;
 }
 
@@ -159,24 +144,8 @@ float FlightLoopSendUDPDatagram(float inElapsedSinceLastCall, float inElapsedTim
     flightModelLon = XPLMGetDatad(DataRefFlightModelLon);
     flightModelElev = XPLMGetDatad(DataRefFlightModelElev);
 
-    flightModelTrueTheta = XPLMGetDataf(DataRefFlightModelTrueTheta);
-    flightModelTruePhi = XPLMGetDataf(DataRefFlightModelTruePhi);
-    flightModelTruePsi = XPLMGetDataf(DataRefFlightModelTruePsi);
-    flightModelMagPsi = XPLMGetDataf(DataRefFlightModelMagPsi);
-
-    flightModelLocalVx = XPLMGetDataf(DataRefFlightModelLocalVx);
-    flightModelLocalVy = XPLMGetDataf(DataRefFlightModelLocalVy);
-    flightModelLocalVz = XPLMGetDataf(DataRefFlightModelLocalVz);
-
-    flightModelLocalAx = XPLMGetDataf(DataRefFlightModelLocalAx);
-    flightModelLocalAy = XPLMGetDataf(DataRefFlightModelLocalAy);
-    flightModelLocalAz = XPLMGetDataf(DataRefFlightModelLocalAz);
-
     QByteArray dataToSend;
     dataToSend.clear();
-    //    dataToSend.append((const char*)&flightModelLat);
-    //    dataToSend.append((const char*)&flightModelLon);
-    //    dataToSend.append((const char*)&flightModelElev);
     QString separator = "; ";
     dataToSend.append(QString::number(flightModelLat, 'f', 6).toUtf8());
     dataToSend.append(separator.toUtf8());
@@ -201,4 +170,13 @@ float FlightLoopCheckDatagramReceived(float inElapsedSinceLastCall, float inElap
         UDPSocketSecondarySender->writeDatagram(datagramToSend);
     }
     return -1;
+}
+
+void DebugToXPlaneLog(QString debugString)
+{
+    QString dbg;
+    dbg = QDateTime::currentDateTime().toString("dd-MM-yyyy, hh'h' mm'min' ss's': ");
+    dbg+= "[" + QString::fromStdString(pluginSignature) + "]: ";
+    dbg+= debugString + "\n";
+    XPLMDebugString(dbg.toStdString().c_str());
 }
